6d6ebd2f2a4b2f2a284521a34dbaca24d9d7b14f
Khoa <trand.dangkhoa84@gmail.com>
2022-08-21 12:03:05 +0700

    Add files via upload

---
+++ bt_chuong_01/02_dice/main.js
@@ -0,0 +1,12 @@
+const images = ["dice1.png","dice2@2x.png","dice3@2x.png","dice4@2x.png","dice5@2x.png","dice6@2x.png"];
+const btn = document.getElementsByClassName("roll-dice")[0];
+const dice1 = document.getElementsByClassName('dice-1')[0];
+const dice2 = document.getElementById('dice-2');
+
+btn.addEventListener("click", (e) => {
+   let img1Index = Math.floor(Math.random() * images.length);
+   let img2Index = Math.floor(Math.random() * images.length);
+
+   dice1.src = "./images/" + images[img1Index];
+   dice2.src = "./images/" + images[img2Index];
+});
