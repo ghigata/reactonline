58e2a9fc888d31a4939e2f210c21737a7071cb29
Khoa <trand.dangkhoa84@gmail.com>
2022-08-21 12:02:45 +0700

    Delete main.js

--- bt_chuong_01/02_dice/main.js
+++
@@ -1,9 +0,0 @@
-let images = ["dice1.png","dice2@2x.png","dice3@2x.png","dice4@2x.png","dice5@2x.png","dice6@2x.png"];
-
-document.getElementsByClassName("roll-dice")[0].addEventListener("click", (e) => {
-   let img1Index = Math.floor(Math.random() * images.length);
-   let img2Index = Math.floor(Math.random() * images.length);
-
-   document.getElementsByClassName('dice-1')[0].src = "./images/" + images[img1Index];
-   document.getElementById('dice-2').src = "./images/" + images[img2Index];
-});
